4eb04198f3476570cc7bfdc3d2f9a6b68c85879a
Dungsenpai-ux <phamtrungtruc03@gmail.com>
2025-09-22 19:54:41 +0700

    first time

--- api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  console.log('API is running on http://localhost:3001')
-  await app.listen(3001)
-}
-bootstrap()
